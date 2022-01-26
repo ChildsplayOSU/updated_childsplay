9ede5c0de5cff6732715bcd3fb12b26c8b89aabd
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2022-01-25 23:25:58 -0800

    Added a google form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,8 +12,10 @@ function Contact(){
             </Container>
             <Container className='section'>
                 <h1 className='headerFonts' style={{fontSize: 46}}> Contact </h1>
-                <p>We’re happy to provide our educational material to you upon request.<br></br><br></br>
-                Please reach out to us at <a href='mailto: parhammj@oregonstate.edu'>parhammj@oregonstate.edu</a> with your name and a description of what you would like to use our curriculum for. We’re also happy to answer any questions you may have as well.</p>
+                <p>We’re happy to provide our educational material to you upon request. Please fill out the following form to receive curriculum:<br></br><br></br>
+                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeqMPt3_FRNojkGPW1kLEyp45BdtbUSy4ndYm-e8Dr7czxQ4w/viewform?embedded=true" width="640" height="760" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                
+                <br></br><br></br>Please reach out to us at <a href='mailto: parhammj@oregonstate.edu'>parhammj@oregonstate.edu</a> with any additional questions you may have as well.</p>
             </Container>
         </div>
     );
